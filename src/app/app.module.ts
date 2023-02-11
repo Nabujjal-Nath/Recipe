7fb6f8b72bbb594510472448df8a30564df4dd0a
Nabujjal-Nath <nabujjal@gmail.com>
2023-02-11 20:31:52 +0530

    folder-structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
+import { RecipesItemsComponent } from './recipes/recipes-list/recipes-items/recipes-items.component';
+import { ShoppingList } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailsComponent,
+    RecipesItemsComponent,
+    ShoppingList,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core';
+
+// import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-header',
+    templateUrl:'./header.component.html',
+    styleUrls:['./header.css']
+}) //decorator
+export class HeaderComponent{
+
+}
